6fc2464363ec45214dab5ccb0c131b9ee22d5efd
Stephanyy <santosstephany400@gmail.com>
2024-05-02 18:17:29 -0300

    Create the component and the style of products component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { NgOptimizedImage } from '@angular/common';
 import { BtnComponent } from '../btn/btn.component';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({ 
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, BtnComponent],
+  imports: [HeaderComponent, NgOptimizedImage, BtnComponent, ProductsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,9 +1,12 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { BtnComponent } from '../btn/btn.component';
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage, BtnComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
